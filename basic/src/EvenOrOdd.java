dffc911cf23b7100c05a42d2b683876be3f527fc
Abash223 <abashnaorem@gmail.com>
2025-09-15 23:21:04 +0530

    Checking even or odd

---
+++ basic/src/EvenOrOdd.java
@@ -0,0 +1,8 @@
+public class EvenOrOdd {
+    public static void main (String[]args){
+      int a = new java.util.Scanner(System.in).nextInt();
+        System.out.println((a%2==0)?"even":"odd");
+
+      }
+    }
+
